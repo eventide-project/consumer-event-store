7bd73b5d16f58ad368a7af3a7824ac92e055490a
Nathan Ladd <nathan@obsidianexchange.com>
2017-01-23 15:31:24 -0600

    Require messaging/event_source

--- lib/consumer/event_store.rb
+++ lib/consumer/event_store.rb
@@ -1,5 +1,5 @@
 require 'consumer'
-require 'event_source/event_store/http'
+require 'messaging/event_source'
 
 require 'consumer/event_store/event_store'
 require 'consumer/event_store/subscription'
