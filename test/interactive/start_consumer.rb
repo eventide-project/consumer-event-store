60a1ba83788c9a0a59500409425126942b6c825d
ntl <nathanladd+github@gmail.com>
2017-05-26 14:36:18 -0500

    Style fixes to interactive test

--- test/interactive/start_consumer.rb
+++ test/interactive/start_consumer.rb
@@ -3,5 +3,5 @@ require_relative './interactive_init'
 category = ENV['CATEGORY'] || 'testEventStoreConsumer'
 
 Actor::Supervisor.start do
-  Controls::Consumer::Incrementing.start "$ce-#{category}"
+  Controls::Consumer::Incrementing.start("$ce-#{category}", position_update_interval: 5)
 end
--- test/interactive/write_messages_periodically.rb
+++ test/interactive/write_messages_periodically.rb
@@ -9,7 +9,7 @@ period = (ENV['PERIOD'] || 300).to_i
 logger = Log.get __FILE__
 
 (1..4).to_a.cycle do |stream_id|
-  stream_name = EventSource::StreamName.stream_name category, stream_id
+  stream_name = MessageStore::StreamName.stream_name(category, stream_id)
 
   message_Data = Controls::MessageData.example
 
